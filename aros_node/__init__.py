a2a966bcf112867582190e806d934e7da8fce147
MMathisLab <mathis@rowland.harvard.edu>
2024-10-28 00:03:47 +0100

    versioning

--- aros_node/__init__.py
+++ aros_node/__init__.py
@@ -1,3 +1,5 @@
-#© M.W. Mathis Lab | Hossein Mirzaei & M.W. Mathis
-# https://github.com/https://github.com/AdaptiveMotorControlLab/AROS
+# © M.W. Mathis Lab | Hossein Mirzaei & M.W. Mathis
+# https://github.com/AdaptiveMotorControlLab/AROS
 # Licensed under Apache 2.0
+
+from aros_node.version import __version__
